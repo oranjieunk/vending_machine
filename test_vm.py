dbd1f560e0b9c6760b22401149b7940baa0aef85
oranjieunk <jieun.kim0413@gmail.com>
2018-08-14 17:11:20 +0900

    First Commit

--- test_vm.py
+++ test_vm.py
@@ -1,2 +1,24 @@
-def test_5th_should_be_5():
-    assert 5 == fibo(33)
+# def test_5th_should_be_5():
+#     assert 5 == fibo(33)
+
+from vm import VendingMachine
+
+
+def test_initial_change_should_be_zero():
+    m = VendingMachine()
+    assert "잔액은 0원입니다" == m.run("잔액")
+
+def test_insert_coin_and_check():
+    m = VendingMachine()
+    assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
+
+def test_accumulation_of_change():
+    m = VendingMachine()
+    m.run("동전 100")
+    m.run("동전 100")
+    assert "잔액은 200원입니다" == m.run("잔액")
+
+def test_unknown_command():
+    m = VendingMachine()
+    assert "알 수 없는 명령입니다" == m.run("웅앵")
--- vm.py
+++ vm.py
@@ -1,9 +1,48 @@
-def test_plus():
-    assert 3 == 1 + 2
+# def test_plus():
+#     assert 3 == 1 + 2
+#
+# def fibo(n):
+#     if n < 3:
+#         return 1
+#     else:
+#         return fibo(n - 2) + fibo(n - 1)
+#     return
 
-def fibo(n):
-    if n < 3:
-        return 1
-    else:
-        return fibo(n - 2) + fibo(n - 1)
-    return
+# change = 0
+#
+# def init():
+#     global change
+#     change = 0
+#
+# def run(cmd):
+#     global change
+#     tokens = cmd.split(" ")
+#     cmd, params = tokens[0], tokens[1:]
+#     if cmd not in ["동전","잔액"]:
+#         return "알 수 없는 명령입니다."
+
+    # if cmd == "잔액":
+    #     return "잔액은 " + str(change) + "원입니다"
+    # elif cmd == "동전":
+    #     coin = params[0]
+    #     change = change + int(coin)
+    #     return coin + "원을 넣었습니다"
+    # else:
+    #     return "알 수 없는 명령입니다."
+
+class VendingMachine:
+    def __init__(self):
+        self._change = 0
+
+    def run(self, raw):
+        tokens = raw.split(" ")
+        cmd, params = tokens[0], tokens[1:]
+
+        if cmd == "잔액":
+            return "잔액은 " + str(self._change) + "원입니다"
+        elif cmd == "동전":
+            coin = params[0]
+            self._change += int(coin)
+            return coin + "원을 넣었습니다"
+        else:
+            return "알 수 없는 명령입니다"
