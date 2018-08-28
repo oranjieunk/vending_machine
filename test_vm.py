cc6c6043a4ed716f8f89a4662403f411b3ddd298
oranjieunk <jieun.kim0413@gmail.com>
2018-08-28 12:36:21 +0900

    Update vending machine

--- test_vm.py
+++ test_vm.py
@@ -1,6 +1,3 @@
-# def test_5th_should_be_5():
-#     assert 5 == fibo(33)
-
 from vm import VendingMachine
 
 
@@ -19,6 +16,51 @@ def test_accumulation_of_change():
     m.run("동전 100")
     assert "잔액은 200원입니다" == m.run("잔액")
 
-def test_unknown_command():
+def test_full():
     m = VendingMachine()
-    assert "알 수 없는 명령입니다" == m.run("웅앵")
+    m.run("동전 500")
+    assert "커피가 나왔습니다." == m.run("음료 커피")
+    assert "잔액은 350원입니다" == m.run("잔액")
+
+def test_unknown_drink():
+    m = VendingMachine()
+    m.run("동전 500")
+    assert "알 수 없는 음료입니다." == m.run("음료 맥주")
+    assert "잔액은 500원입니다" == m.run("잔액")
+
+def test_short():
+    m = VendingMachine()
+    m.run("동전 100")
+    assert "잔액이 부족합니다." == m.run("음료 커피")
+    assert "잔액은 100원입니다" == m.run("잔액")
+
+def test_invalid_coin():
+    m = VendingMachine()
+    assert "알 수 없는 동전입니다." == m.run("동전 999")
+    assert "잔액은 0원입니다" == m.run("잔액")
+
+def test_I_dont_know():
+    m = VendingMachine()
+    assert "알 수 없는 명령입니다." == m.run("moony")
+
+def test_milk_and_milkcoffee():
+    m = VendingMachine()
+    m.run("동전 500")
+    assert "우유가 나왔습니다." == m.run("음료 우유")
+    assert "밀크커피가 나왔습니다." == m.run("음료 밀크커피")
+
+def test_return():
+    m = VendingMachine()
+    m.run("동전 500")
+    m.run("동전 100")
+    m.run("동전 50")
+    m.run("동전 10")
+    m.run("동전 10")
+    assert "500,100,50,10,10" == m.run("반환")
+    assert "잔액이 없습니다." == m.run("반환")
+
+# def test_valid_coins():
+#     m = VendingMachine()
+#     valid_coins = ["10", "50", "100", "500"]
+#     for coin in valid_coins:
+#         assert coin + "원을 넣었습니다" = m.run("동전" + coin)
--- vm.py
+++ vm.py
@@ -1,48 +1,81 @@
-# def test_plus():
-#     assert 3 == 1 + 2
-#
-# def fibo(n):
-#     if n < 3:
-#         return 1
-#     else:
-#         return fibo(n - 2) + fibo(n - 1)
-#     return
-
-# change = 0
-#
-# def init():
-#     global change
-#     change = 0
-#
-# def run(cmd):
-#     global change
-#     tokens = cmd.split(" ")
-#     cmd, params = tokens[0], tokens[1:]
-#     if cmd not in ["동전","잔액"]:
-#         return "알 수 없는 명령입니다."
-
-    # if cmd == "잔액":
-    #     return "잔액은 " + str(change) + "원입니다"
-    # elif cmd == "동전":
-    #     coin = params[0]
-    #     change = change + int(coin)
-    #     return coin + "원을 넣었습니다"
-    # else:
-    #     return "알 수 없는 명령입니다."
-
 class VendingMachine:
     def __init__(self):
         self._change = 0
 
     def run(self, raw):
         tokens = raw.split(" ")
-        cmd, params = tokens[0], tokens[1:]
+        cmd, params = tokens[0], tokens[1:]   # ex)["동전","100","잔액"]
 
         if cmd == "잔액":
             return "잔액은 " + str(self._change) + "원입니다"
         elif cmd == "동전":
+            known_coins = "10,50,100,500".split(",")
             coin = params[0]
+            if coin not in known_coins:
+                return "알 수 없는 동전입니다."
+
             self._change += int(coin)
             return coin + "원을 넣었습니다"
+
+        elif cmd == "음료":
+            prices = {'커피': 150, '우유': 200, '밀크커피': 300}
+            beverage = params[0]
+            known_beverages = list(prices.keys())
+            if beverage not in known_beverages:
+                return "알 수 없는 음료입니다."
+            if self._change < prices[beverage]:
+                return "잔액이 부족합니다."
+
+            self._change = self._change - prices[beverage]
+            return beverage + "가 나왔습니다."
+
+        elif cmd == "반환":
+            coins = [500,100,50,10]
+            changes = ""
+            i = 0
+            if self._change == 0:
+                changes = "잔액이 없습니다.."
+            while self._change > 0:
+                if self._change // coins[i] > 0:
+                    changes = changes + str(coins[i]) + ","
+                    self._change = self._change - coins[i]
+                else:
+                    i = i+1
+            changes = changes[:-1]
+            return changes
+
+
+
+        # elif cmd == "음료":
+        #     drink = params[0]
+        #     if drink != "커피":
+        #         return "알 수 없는 음료입니다."
+        #     else:
+        #         price = 150
+        #         if self._change >= price:
+        #             self._change = self._change - price
+        #             return "커피가 나왔습니다."
+        #         else :
+        #             return "잔액이 부족합니다."
         else:
-            return "알 수 없는 명령입니다"
+            return "알 수 없는 명령입니다."
+
+
+# def init():
+#     global change
+#     change = 0
+#
+# def run(raw):
+#     global change
+#
+#     tokens = raw.split(" ")
+#     cmd, params = tokens[0], tokens[1:]
+#
+#     if cmd == "잔액":
+#         return "잔액은 " + str(change) + "원입니다"
+#     elif cmd == "동전":
+#         coin = params[0]
+#         change += int(coin)
+#         return coin + "원을 넣었습니다"
+#     else:
+#         return "알 수 없는 명령입니다."
